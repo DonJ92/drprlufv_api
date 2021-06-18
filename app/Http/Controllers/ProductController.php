88132beb50afe0d8354f3705e45fa5c834211043
alim-razi1028 <alim.razi1028@gmail.com>
2021-06-18 16:38:57 +0900

    Update Products API

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Requests\ProductRequest;
@@ -40,7 +41,8 @@ class ProductController extends Controller
         if ($validator->fails()) {
             return $this->respondValidateError($validator->errors()->first());
         }
-        
+
+        $data = $request->all();
         $latitude = $request->latitude;
         $longitude = $request->longitude;
 		$keyword = $request->keyword;
@@ -48,64 +50,67 @@ class ProductController extends Controller
         if (empty($count)) 
             $count = 3;
 
-        if (is_double($latitude) && is_double($longitude)) {	        
-			$distanceField = "111.111 * DEGREES(ACOS(LEAST(1.0, COS(RADIANS(latitude)) * COS(RADIANS({$latitude})) * COS(RADIANS(longitude - ${longitude}))" . " + SIN(RADIANS(latitude)) * SIN(RADIANS({$latitude}))))) AS distance"; 
+        try {
+            if (is_double($latitude) && is_double($longitude)) {
+                $distanceField = "111.111 * DEGREES(ACOS(LEAST(1.0, COS(RADIANS(latitude)) * COS(RADIANS({$latitude})) * COS(RADIANS(longitude - ${longitude}))" . " + SIN(RADIANS(latitude)) * SIN(RADIANS({$latitude}))))) AS distance";
 
-	        $query = Product::select('products.*', 'users.image as user_icon', DB::raw($distanceField))
-	                        ->leftjoin('users', 'users.id', '=', 'products.user_id')
-	                        ->orderBy('distance', 'ASC')
-	                        ->orderBy('likes', 'DESC')
-	                        ->orderBy('created_at', 'DESC')
-	                        ->orderBy(DB::raw('RAND()'));
+                $query = Product::select('products.*', 'users.image as user_icon', DB::raw($distanceField))
+                    ->leftjoin('users', 'users.id', '=', 'products.user_id')
+                    ->orderBy('distance', 'ASC')
+                    ->orderBy('likes', 'DESC')
+                    ->orderBy('created_at', 'DESC')
+                    ->orderBy(DB::raw('RAND()'));
 
-			if (!empty($keyword)) {
-				$query = $query->where(function($q) use($keyword) {
-					$q->where('products.title', 'like', '%'.$keyword.'%')
-						->orWhere('products.description', 'like', '%'.$keyword.'%');
-				});
-			}
-			$rows = $query->get();
-        } else {
-        	$query = Product::select('products.*', 'users.image as user_icon')
-	                        ->leftjoin('users', 'users.id', '=', 'products.user_id')
-	                        ->orderBy('likes', 'DESC')
-	                        ->orderBy('created_at', 'DESC')
-	                        ->orderBy(DB::raw('RAND()'));
+                if (!empty($keyword)) {
+                    $query = $query->where(function ($q) use ($keyword) {
+                        $q->where('products.title', 'like', '%' . $keyword . '%')
+                            ->orWhere('products.description', 'like', '%' . $keyword . '%');
+                    });
+                }
+                $rows = $query->get();
+            } else {
+                $query = Product::select('products.*', 'users.image as user_icon')
+                    ->leftjoin('users', 'users.id', '=', 'products.user_id')
+                    ->orderBy('likes', 'DESC')
+                    ->orderBy('created_at', 'DESC')
+                    ->orderBy(DB::raw('RAND()'));
 
-			if (!empty($keyword)) {
-				$query = $query->where(function($q) use($keyword) {
-					$q->where('products.title', 'like', '%'.$keyword.'%')
-						->orWhere('products.description', 'like', '%'.$keyword.'%');
-				});
-			}
-			$rows = $query->get();
-	
-        }
-        
-        $max = count($rows) - 1;
-
-        $result = array();
-
-        while(count($result) < $count) {
-            $index = $this->random_select(0, $max * 2, $max / 3 * 2);
-            $index = abs($max - $index);
-            $row = $rows[$index];
-            if (in_array($row, $result))
-                continue;
-            array_push($result, $row);
-        };        
-        
-        foreach ($result as $row) {            
-            $filenames = [];
-
-            $images = DB::table('images')->where('product_id', '=', $row->id)->get();
-            foreach ($images as $image) {
-                array_push($filenames, $image->filename);
+                if (!empty($keyword)) {
+                    $query = $query->where(function ($q) use ($keyword) {
+                        $q->where('products.title', 'like', '%' . $keyword . '%')
+                            ->orWhere('products.description', 'like', '%' . $keyword . '%');
+                    });
+                }
+                $rows = $query->get();
             }
 
-            $row->filenames = $filenames;
+            $max = count($rows) - 1;
 
-            $row->comments = DB::table('comments')->where('product_id', '=', $row->id)->count();
+            $result = array();
+
+            while (count($result) < $count && count($result) != count($rows)) {
+                $index = $this->random_select(0, $max * 2, $max / 3 * 2);
+                $index = abs($max - $index);
+                $row = $rows[$index];
+                if (in_array($row, $result))
+                    continue;
+                array_push($result, $row);
+            };
+
+            foreach ($result as $row) {
+                $filenames = [];
+
+                $images = DB::table('images')->where('product_id', '=', $row->id)->get();
+                foreach ($images as $image) {
+                    array_push($filenames, $image->filename);
+                }
+
+                $row->filenames = $filenames;
+
+                $row->comments = DB::table('comments')->where('product_id', '=', $row->id)->count();
+
+            }
+        } catch(QueryException $e) {
 
         }
         
